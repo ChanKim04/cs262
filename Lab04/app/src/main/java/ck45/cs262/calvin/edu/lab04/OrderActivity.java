33369243a7a6e177507f1be9baaacf2a35d4dbc8
ChanKim04 <kimchan04@gmail.com>
2018-09-28 17:44:01 -0400

    Lab04

---
+++ Lab04/app/src/main/java/ck45/cs262/calvin/edu/lab04/OrderActivity.java
@@ -0,0 +1,17 @@
+package ck45.cs262.calvin.edu.lab04;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class OrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+    }
+
+    public void onRadioButtonClicked(View view) {
+    }
+}
