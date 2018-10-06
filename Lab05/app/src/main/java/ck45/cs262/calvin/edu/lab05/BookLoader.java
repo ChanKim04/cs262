43d3b4e75a7e7d979a41d51f198aeab288d95dff
ChanKim04 <kimchan04@gmail.com>
2018-10-05 22:47:06 -0400

    Lab05

---
+++ Lab05/app/src/main/java/ck45/cs262/calvin/edu/lab05/BookLoader.java
@@ -0,0 +1,27 @@
+package ck45.cs262.calvin.edu.lab05;
+
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v4.content.AsyncTaskLoader;
+
+public class BookLoader extends AsyncTaskLoader<String> {
+
+    private String mQueryString;
+
+    public BookLoader(@NonNull Context context, String queryString) {
+        super(context);
+        mQueryString = queryString;
+    }
+
+    @Override
+    protected void onStartLoading() {
+        super.onStartLoading();
+        forceLoad();
+    }
+
+    @Override
+    public String loadInBackground() {
+        return NetworkUtils.getBookInfo(mQueryString);
+    }
+}
